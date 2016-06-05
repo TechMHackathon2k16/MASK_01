9dc267764693dcd60c2d838d9e1136d94deb2eb9
Mukesh Yadav <mukeshyadav@InnovationM-64.local>
2016-06-05 10:14:26 +0530

    Adding to Org

---
+++ app/src/main/java/com/mask/model/ComplaintModel.java
@@ -0,0 +1,7 @@
+package com.mask.model;
+
+/**
+ * Created by mukeshyadav on 05/06/16.
+ */
+public class ComplaintModel {
+}
---
+++ app/src/main/java/com/mask/network/WebServiceManager.java
@@ -0,0 +1,7 @@
+package com.mask.network;
+
+/**
+ * Created by mukeshyadav on 05/06/16.
+ */
+public class WebServiceManager {
+}
