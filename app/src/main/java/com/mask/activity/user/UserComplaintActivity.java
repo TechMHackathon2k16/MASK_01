4bd6f0620b89cd713a45badec6f843b45977586e
Mukesh Yadav <mukeshyadav@InnovationM-64.local>
2016-06-05 09:49:09 +0530

    Added Fragments

---
+++ app/src/main/java/com/mask/activity/user/UserComplaintActivity.java
@@ -0,0 +1,13 @@
+package com.mask.activity.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserComplaintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_complaint);
+    }
+}
