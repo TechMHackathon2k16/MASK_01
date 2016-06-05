67e65ead55521e63efbb3b0faeb6aaf1ab9c747b
Shahroz <shahrozbeg@gmail.com>
2016-06-05 11:22:42 +0530

    nnn

--- app/src/main/java/com/mask/model/DepartmentModel.java
+++ app/src/main/java/com/mask/model/DepartmentModel.java
@@ -4,4 +4,35 @@ package com.mask.model;
  * Created by mukeshyadav on 05/06/16.
  */
 public class DepartmentModel {
+
+    int deptId;
+    int modId;
+    String deptName;
+
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(int deptId) {
+        this.deptId = deptId;
+    }
+
+    public int getModId() {
+        return modId;
+    }
+
+    public void setModId(int modId) {
+        this.modId = modId;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+
+
 }
